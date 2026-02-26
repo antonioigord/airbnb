f3f8d423a36111375860297ecc2fb99fe7134ebd
antonioigord <antonioigord@gmail.com>
2026-02-26 15:07:41 -0300

    adicionando stimulus js no header

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,25 @@
+// hello_controller.js
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+
+export default class extends Controller {
+    static targets = ["dropdown","openUserMenu"]
+    connect() {
+        this.openUserMenuTarget.addEventListener("click", (e)=> {
+            //console.log("Header clicked");})
+            openDropdown(this.dropdownTarget)
+            
+        })
+    }
+}
+
+function openDropdown(element) {
+  toggle(element).then(() => {
+    console.log("Enter transition complete")
+  })
+}
+function closeDropdown(element) {
+  leave(element).then(() => {
+      element.destroy();
+  })
+}
